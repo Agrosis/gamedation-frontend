86d3645ad54eb57fcb3b41cfb62089cbfc86213c
Muhtasim Ahsan <agro@jantox.com>
2015-05-31 13:53:19 -0500

    header and game item changes.

--- javascripts/components/Header.js
+++ javascripts/components/Header.js
@@ -6,7 +6,17 @@ var Header = React.createClass({
   render: function(){
     return (
       <div className="header">
-        
+        <div className="header-content">
+          <div className="header-nav">
+            Home
+          </div>
+          <div className="header-nav">
+            Games
+          </div>
+          <div className="header-nav nav-right">
+            Sign out
+          </div>
+        </div>
       </div>
     );
   }
